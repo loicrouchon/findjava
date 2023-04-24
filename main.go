2b58c99f47b4807000c7f050194beb51aee63b3f
Loic Rouchon <loic@loicrouchon.com>
2023-04-24 11:28:57 +0200

    Implement version range parsing

--- main.go
+++ main.go
@@ -8,8 +8,28 @@ import (
 	"os/exec"
 	"os/user"
 	"path/filepath"
+	"regexp"
+	"strconv"
 	"strings"
 )
+
+type JvmSelectionRules struct {
+	versionRange  string
+	minJvmVersion int
+	maxJvmVersion int
+}
+
+func (rules JvmSelectionRules) String() string {
+	return fmt.Sprintf(
+		`{
+            versionRange: %s
+            minJvmVersion: %d
+            maxJvmVersion: %d
+}`,
+		rules.versionRange,
+		rules.minJvmVersion,
+		rules.maxJvmVersion)
+}
 
 type JvmInfo struct {
 	javaPaths                []string
@@ -30,7 +50,57 @@ func (jvmInfo JvmInfo) String() string {
 }
 
 func main() {
-	setLogLevel()
+	args := parseArgs()
+	if len(args) > 1 {
+		logError("Usage jvm-finder [VERSION]")
+		logError("  VERSION: A JVM version range:")
+		logError("      - 17        exact version)")
+		logError("      - 17..      17 or above)")
+		logError("      - ..17      up to 17")
+		logError("      - 11..17    From 11 to 17")
+		os.Exit(1)
+	}
+	jvmVersionRange := args[0]
+	logInfo("%s", jvmVersionRange)
+	jvmVersionRegex := `[\d]+(?:\.[\d]+)*`
+	r := regexp.MustCompile(fmt.Sprintf(`^(?:`+
+		`(?P<exact>%[1]s)`+
+		`|(?:(?P<min>%[1]s)\.\.)`+
+		`|(?:\.\.(?P<max>%[1]s))`+
+		`|(?:(?P<min>%[1]s)\.\.(?P<max>%[1]s))`+
+		`)$`,
+		jvmVersionRegex))
+	groupNames := r.SubexpNames()
+	for _, input := range []string{
+		"1.8",
+		"..1.8",
+		"1.8..",
+		"1.8..17",
+	} {
+		match := r.FindStringSubmatch(input)
+		var minJvmVersion int
+		var maxJvmVersion int
+		for i, m := range match {
+			if len(m) > 0 {
+				switch groupNames[i] {
+				case "exact":
+					minJvmVersion = parseVersion(m)
+					maxJvmVersion = parseVersion(m)
+				case "min":
+					minJvmVersion = parseVersion(m)
+				case "max":
+					maxJvmVersion = parseVersion(m)
+				}
+			}
+		}
+		rules := JvmSelectionRules{
+			versionRange:  input,
+			minJvmVersion: minJvmVersion,
+			maxJvmVersion: maxJvmVersion,
+		}
+		logInfo("%s", rules)
+	}
+
 	var javaLookUpPaths = []string{
 		"/bin/java",
 		"/usr/bin/java",
@@ -44,6 +114,34 @@ func main() {
 		jvmInfo := jvmInfo(javaPath, javaSymLinks)
 		jvmInfos[javaPath] = jvmInfo
 		logInfo("%s: %s", javaPath, jvmInfo)
+	}
+}
+
+func parseVersion(version string) int {
+	switch version {
+	case "1.0", "1.1":
+		return 1
+	case "1.2":
+		return 2
+	case "1.3":
+		return 3
+	case "1.4":
+		return 4
+	case "1.5":
+		return 5
+	case "1.6":
+		return 6
+	case "1.7":
+		return 7
+	case "1.8":
+		return 8
+	default:
+		v, err := strconv.Atoi(version)
+		if err != nil {
+			logError("JVM version %s cannot be parsed as an int")
+			panic(version)
+		}
+		return v
 	}
 }
 
@@ -151,13 +249,15 @@ func jvmInfo(javaPath string, javaSymLinks []string) JvmInfo {
 }
 
 var logLevel string
-func setLogLevel() {
+
+func parseArgs() []string {
 	flag.StringVar(&logLevel, "loglevel", "error", "Log level: info, error")
 	flag.Parse()
-    if (logLevel != "info" && logLevel != "error") {
+	if logLevel != "info" && logLevel != "error" {
 		logError("Invalid log level: '%s'. Available levels are: info, error", logLevel)
-        os.Exit(1)
+		os.Exit(1)
 	}
+	return flag.Args()
 }
 
 func logInfo(message string, v ...any) {
