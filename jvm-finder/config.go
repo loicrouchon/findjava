398b7430003fff4454d823726318f6b2d166eb80
Loic Rouchon <loic@loicrouchon.com>
2023-04-27 16:31:43 +0200

    Refactor config to allow override

--- jvm-finder/config.go
+++ jvm-finder/config.go
@@ -3,11 +3,34 @@ package main
 import (
 	"encoding/json"
 	"os"
+	"strings"
 )
 
+const defaultKey = "default"
+
+var defaultConfigEntry = ConfigEntry{
+	path: "<DEFAULT>",
+	JvmLookupPaths: []string{
+		"/bin/java",
+		"/usr/bin/java",
+		"/usr/local/bin/java",
+		"/usr/lib/jvm",
+		"~/.sdkman/candidates/java",
+	},
+	JvmVersionRange: &VersionRange{
+		Min: 0,
+		Max: 0,
+	},
+}
+
 type Config struct {
-	JvmLookupPaths         []string
-	DefaultJvmVersionRange VersionRange
+	configs []ConfigEntry
+}
+
+type ConfigEntry struct {
+	path            string
+	JvmLookupPaths  []string
+	JvmVersionRange *VersionRange
 }
 
 type VersionRange struct {
@@ -15,27 +38,61 @@ type VersionRange struct {
 	Max int
 }
 
-func loadConfig(path string) *Config {
-	var config *Config
-	if _, err := os.Stat(path); err == nil {
-		file, _ := os.Open(path)
-		defer file.Close()
-		decoder := json.NewDecoder(file)
-		config = &Config{}
-		err := decoder.Decode(config)
-		if err != nil {
-			dierr(err)
-		}
-	} else {
-		config = &Config{
-			JvmLookupPaths: []string{
-				"/bin/java",
-				"/usr/bin/java",
-				"/usr/local/bin/java",
-				"/usr/lib/jvm",
-				"~/.sdkman/candidates/java",
-			},
+func (config *Config) paths() []string {
+	var paths []string
+	for _, cfg := range config.configs {
+		paths = append(paths, cfg.path)
+	}
+	return paths
+}
+
+func (config *Config) jvmLookupPaths() *[]string {
+	for _, cfg := range config.configs {
+		if len(cfg.JvmLookupPaths) > 0 {
+			return &cfg.JvmLookupPaths
 		}
 	}
+	die("no JVM lookup path defined in configuration files %v", config.paths())
+	panic("unreachable")
+}
+
+func (config *Config) jvmVersionRange() *VersionRange {
+	for _, cfg := range config.configs {
+		if cfg.JvmVersionRange != nil {
+			return cfg.JvmVersionRange
+		}
+	}
+	die("no JVM Version range defined in configuration files %v", config.paths())
+	panic("unreachable")
+}
+
+func loadConfig(defaultConfigPath string, name string) *Config {
+	config := &Config{}
+	var configPaths []string
+	if name != defaultKey {
+		specificConfigPath := strings.TrimSuffix(defaultConfigPath, ".json") + "." + name + ".json"
+		configPaths = []string{specificConfigPath, defaultConfigPath}
+	} else {
+		configPaths = []string{defaultConfigPath}
+	}
+	for _, path := range configPaths {
+		if _, err := os.Stat(path); err == nil {
+			logDebug("Loading config from %s", path)
+			configEntry := ConfigEntry{
+				path: path,
+			}
+			file, _ := os.Open(path)
+			defer file.Close()
+			decoder := json.NewDecoder(file)
+			err := decoder.Decode(&configEntry)
+			if err != nil {
+				dierr(err)
+			}
+			config.configs = append(config.configs, configEntry)
+		} else {
+			logDebug("Config file %s not found: %v", path, err)
+		}
+	}
+	config.configs = append(config.configs, defaultConfigEntry)
 	return config
 }
--- jvm-finder/config_test.go
+++ jvm-finder/config_test.go
@@ -6,48 +6,105 @@ import (
 )
 
 func TestLoadConfig(t *testing.T) {
-	data := map[string]Config{
+	defaultJvmLookupPath := []string{
+		"/bin/java",
+		"/usr/bin/java",
+		"/usr/local/bin/java",
+		"/usr/lib/jvm",
+		"~/.sdkman/candidates/java",
+	}
+	defaultJvmVersionRange := &VersionRange{
+		Min: 0,
+		Max: 0,
+	}
+	data := map[string]ConfigEntry{
 		"test-resources/missing-config.json": {
-			JvmLookupPaths: []string{
-				"/bin/java",
-				"/usr/bin/java",
-				"/usr/local/bin/java",
-				"/usr/lib/jvm",
-				"~/.sdkman/candidates/java",
-			},
+			JvmLookupPaths:  defaultJvmLookupPath,
+			JvmVersionRange: defaultJvmVersionRange,
 		},
-		"test-resources/empty-config.json": {},
+		"test-resources/empty-config.json": {
+			JvmLookupPaths:  defaultJvmLookupPath,
+			JvmVersionRange: defaultJvmVersionRange,
+		},
 		"test-resources/path-lookup-config.json": {
 			JvmLookupPaths: []string{
 				"/usr/bin/java",
 				"/usr/lib/jvm",
 				"~/.sdkman/candidates/java",
 			},
+			JvmVersionRange: defaultJvmVersionRange,
 		},
 		"test-resources/min-jvm-version-config.json": {
-			DefaultJvmVersionRange: VersionRange{
+			JvmLookupPaths: defaultJvmLookupPath,
+			JvmVersionRange: &VersionRange{
 				Min: 8,
 			},
 		},
 		"test-resources/full-config.json": {
 			JvmLookupPaths: []string{
-				"/bin/java",
 				"/usr/bin/java",
-				"/usr/local/bin/java",
 				"/usr/lib/jvm",
 				"~/.sdkman/candidates/java",
 			},
-			DefaultJvmVersionRange: VersionRange{
+			JvmVersionRange: &VersionRange{
 				Min: 8,
 				Max: 17,
 			},
 		},
+		"test-resources/invalid-config.json": {
+			JvmLookupPaths:  defaultJvmLookupPath,
+			JvmVersionRange: defaultJvmVersionRange,
+		},
 	}
-	for path, expectedConfig := range data {
-		actualConfig := loadConfig(path)
-		if !reflect.DeepEqual(*actualConfig, expectedConfig) {
-			t.Fatalf(`Expecting loadConfig("%s") == %v but was %v`,
-				path, expectedConfig, *actualConfig)
+	for path, expectedConfigEntry := range data {
+		actualConfig := loadConfig(path, defaultKey)
+		actualJvmLookupPath := *actualConfig.jvmLookupPaths()
+		if !reflect.DeepEqual(actualJvmLookupPath, expectedConfigEntry.JvmLookupPaths) {
+			t.Fatalf(`Expecting loadConfig("%s", "%s").jvmLookupPaths() == %v but was %v`,
+				path, defaultKey, expectedConfigEntry.JvmLookupPaths, actualJvmLookupPath)
+		}
+		actualJvmVersionRange := *actualConfig.jvmVersionRange()
+		if !reflect.DeepEqual(actualJvmVersionRange, *(expectedConfigEntry.JvmVersionRange)) {
+			t.Fatalf(`Expecting loadConfig("%s", "%s").jvmVersionRange() == %v but was %v`,
+				path, defaultKey, *expectedConfigEntry.JvmVersionRange, actualJvmVersionRange)
+		}
+	}
+}
+
+func TestLoadConfigWithOverrides(t *testing.T) {
+	data := map[string]ConfigEntry{
+		"abc": {
+			JvmLookupPaths: []string{
+				"~/.sdkman/candidates/java",
+			},
+			JvmVersionRange: &VersionRange{
+				Min: 8,
+				Max: 17,
+			},
+		},
+		"xyz": {
+			JvmLookupPaths: []string{
+				"/usr/bin/java",
+				"/usr/lib/jvm",
+				"~/.sdkman/candidates/java",
+			},
+			JvmVersionRange: &VersionRange{
+				Min: 11,
+			},
+		},
+	}
+	for key, expectedConfigEntry := range data {
+		path := "test-resources/full-config.json"
+		actualConfig := loadConfig(path, key)
+		actualJvmLookupPath := *actualConfig.jvmLookupPaths()
+		if !reflect.DeepEqual(actualJvmLookupPath, expectedConfigEntry.JvmLookupPaths) {
+			t.Fatalf(`Expecting loadConfig("%s", "%s").jvmLookupPaths() == %v but was %v`,
+				path, defaultKey, expectedConfigEntry.JvmLookupPaths, actualJvmLookupPath)
+		}
+		actualJvmVersionRange := *actualConfig.jvmVersionRange()
+		if !reflect.DeepEqual(actualJvmVersionRange, *(expectedConfigEntry.JvmVersionRange)) {
+			t.Fatalf(`Expecting loadConfig("%s", "%s").jvmVersionRange() == %v but was %v`,
+				path, defaultKey, *expectedConfigEntry.JvmVersionRange, actualJvmVersionRange)
 		}
 	}
 }
