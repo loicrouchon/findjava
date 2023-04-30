baa7d3331d34ca6acd0f79964e9fd28a865bc16a
Loic Rouchon <loic@loicrouchon.com>
2023-04-30 11:33:01 +0200

    Remove unnecessary closure for closing files

--- jvm-finder/file.go
+++ jvm-finder/file.go
@@ -3,10 +3,8 @@ package main
 import "os"
 
 func closeFile(file *os.File) {
-	func(file *os.File) {
-		err := file.Close()
-		if err != nil {
-			dierr(err)
-		}
-	}(file)
+	err := file.Close()
+	if err != nil {
+		dierr(err)
+	}
 }
