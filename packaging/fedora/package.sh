6c0518725dc931f0dd86673d5156497113f4775d
Loic Rouchon <loic@loicrouchon.com>
2023-05-29 21:05:27 +0200

    Add jvm-finder fedora package.sh script to generate a proper RPM spec for the version

---
+++ packaging/fedora/package.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env sh
+set -e
+#set -x
+
+cur_dir="$(dirname "$(realpath "$0")")"
+
+if [ "$#" -ne 1 ]; then
+  echo "Error: Invalid number of arguments. Expected 1 argument."
+  echo "Usage $0 VERSION"
+  exit 1
+fi
+
+version="$1"
+if [ -z "$package_name" ]; then
+    . "${cur_dir}/../common/config.sh"
+fi
+
+build() {
+    echo "Building fedora source package ${package_version_dir}"
+    sed "s/\${version}/${version}/" "${cur_dir}/jvm-finder.spec" > "build/${package_name}/jvm-finder.spec"
+}
+
+"${cur_dir}/../common/clean.sh" "build" "${package_name}"
+build
